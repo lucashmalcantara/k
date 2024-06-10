1cd803a645bc530aa1dc857fd69d5d7e66553102
Lucas Alcântara <lucashmalcantara@gmail.com>
2024-06-09 22:04:26 -0300

    Quartz sync: Jun 9, 2024, 10:04 PM

---
+++ content/Lucas/About Me.md
@@ -0,0 +1,13 @@
+---
+title: About Me
+draft: false
+tags:
+  - lucas-alcantara
+---
+ 
+- My name is Lucas Alcântara.
+- I'm from Brazil.
+- I have a Bachelor's degree in Information Systems (PUC Minas).
+- Here are my social media accounts:
+    - [GitHub](https://github.com/lucashmalcantara)
+    - [LinkedIn](https://www.linkedin.com/in/lucashmalcantara/)
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Hey! It's Lucas Alcântara's Digital Garden 🌱",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "lucashmalcantara.github.io/k",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
