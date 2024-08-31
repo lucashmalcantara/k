116d8cb703e89236b2d64e4d130071d85d909f4f
Lucas Alcântara <lucashmalcantara@gmail.com>
2024-08-31 14:18:20 -0300

    Quartz sync: Aug 31, 2024, 2:18 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'lucashmalcantara/k',
+        // from data-repo-id
+        repoId: 'R_kgDOMq-eXA',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOMq-eXM4CiHTj',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
